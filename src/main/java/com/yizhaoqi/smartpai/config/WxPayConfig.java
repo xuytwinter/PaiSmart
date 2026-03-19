d028f6e2f32a68c384795c12456a66aa1c733349
xuytwinter <158798505+xuytwinter@users.noreply.github.com>
2026-03-19 20:52:29 +0800

    feat: 兼容支付密钥base64传递方式

--- src/main/java/com/yizhaoqi/smartpai/config/WxPayConfig.java
+++ src/main/java/com/yizhaoqi/smartpai/config/WxPayConfig.java
@@ -1,8 +1,8 @@
 package com.yizhaoqi.smartpai.config;
 
 import lombok.Data;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -45,6 +45,11 @@ public class WxPayConfig {
         if (privateKey != null && privateKey.contains("-----BEGIN PRIVATE KEY")) {
             // 私钥内容是直接以文本的方式提供的，直接返回
             return privateKey;
+        }
+
+        if (privateKey != null && (privateKey.endsWith("=") || privateKey.length() > 200)) {
+            // 如果是base64编码的传入方式, 使用base64进行解码
+            return new String(Base64.decodeBase64(privateKey), StandardCharsets.UTF_8);
         }
 
         // 私钥是以文件的方式提供
